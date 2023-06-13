c454366d5b95222bb1cb0b3fcd061a2c4935fbd9
master <master@SMLAB306PC20_23>
2023-06-12 21:03:19 -0500

    Agregar archivos de proyecto.

---
+++ stoapantaS3/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace stoapantaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string dato1 = txtDatoUno.Text;
+            double dato2 = Convert.ToDouble(txtDatoDos.Text);
+
+            Navigation.PushAsync(new Page2(dato1, dato2));
+        }
+    }
+}
---
+++ stoapantaS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace stoapantaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1, double dato2)
+        {
+            InitializeComponent();
+            lblDatoUno.Text = "Dato uno" + dato1;
+            lblDatoDos.Text = "Dato dos" + dato2.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
